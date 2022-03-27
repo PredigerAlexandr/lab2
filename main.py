621aad44ded6c1ecc9ac39e8ee6cb9046a2fba4c
Alexandr <predigeralexndr@gmail.com>
2022-03-27 23:28:35 +0400

    1 task - ready

--- main.py
+++ main.py
@@ -2,21 +2,32 @@ from io import StringIO
 import numpy as np
 
 productDic = {}
-dicKcal = {}
 KcalCount = -1
-
+sugarCount = 999999999
+proteinCount = -1
+vitaminCcount = -1
 
 table = np.genfromtxt("ABBREV.csv", delimiter=";", dtype=None, names=True, encoding="utf8")
-#строене словаря key-product :: cal - sugar - protein - vitaminC
+
+# строене словаря key-product :: cal - sugar - protein - vitaminC
 for i in range(table.size):
-    productDic[i] = []
-    productDic = [table[i][3]]
-    dicKcal[table[i][1]] = table[i][3]
+    productDic[table[i][1]] = [table[i][3], table[i][9], table[i][4], table[i][20]]
 
-for i in dicKcal:
-    if dicKcal[i] > KcalCount:
-        KcalCount >= dicKcal[i]
-        maxCalProduct = i
+for i in productDic:
+    if productDic[i][0] >= KcalCount:
+        KcalCount = productDic[i][0]
+        maxCalProduct = i + " -- " + str(KcalCount)
 
+    if productDic[i][1] < sugarCount:
+        sugarCount = productDic[i][1]
+        minSugarCount = i + " -- " + str(sugarCount)
 
-print(maxCalProduct, KcalCount)
+    if productDic[i][2] > proteinCount:
+        proteinCount = productDic[i][2]
+        maxProteinProduct = i + " -- " + str(proteinCount)
+
+    if productDic[i][3] > vitaminCcount:
+        vitaminCcount = productDic[i][3]
+        maxVitaminCProduct = i + " -- " + str(vitaminCcount)
+
+print(maxCalProduct, minSugarCount, maxProteinProduct, maxVitaminCProduct, sep='\n')
