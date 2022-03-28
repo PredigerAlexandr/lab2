f8f54e19210315382e2f66702c92dad7de704c60
Alexandr <predigeralexndr@gmail.com>
2022-03-28 15:03:06 +0400

    1 task - ready 3 task - 2 version

--- task3.py
+++ task3.py
@@ -1,4 +1,19 @@
 import os
+from os import walk
 
-print('\n'.join(os.listdir("C:\\Users\\Alexandr\\Desktop\\_\\4 семестр УлГТУ ИВТ\\Технологии программирования\\lab2 Task3")))
+path = "C:\\Users\\Alexandr\\Desktop\\_\\4 семестр УлГТУ ИВТ\\Технологии программирования\\lab2 Task3"
 
+
+def get_files_sizes(dirpath):
+    fileList = []
+    for (dirpath, dirnames, filenames) in walk(path):
+        fileList.extend(filenames)
+        break
+    for i in range(len(fileList)):
+        fileInfo = os.stat(path + "\\" + fileList[i])
+        fileList[i] = fileList[i] + "--" + str(fileInfo.st_size) + "Kb"
+
+    return fileList
+
+
+print("\n".join(get_files_sizes(path)))
