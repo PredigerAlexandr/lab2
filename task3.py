d377694910034c1953f5f65a9fa70516c52b577d
Alexandr <predigeralexndr@gmail.com>
2022-03-30 17:53:46 +0400

    1 task - ready 3 task - 4 version

--- task3.py
+++ task3.py
@@ -1,9 +1,10 @@
 import os
+from operator import itemgetter
 from os import walk
-import numpy as np
+import re
 
-path = "C:\\Users\\Alexandr\\Desktop\\_\\4 семестр УлГТУ ИВТ\\Технологии программирования\\lab2 Task3"
-csvFileFlag = False
+import numpy as np
+import csv
 
 
 def get_files_sizes(dirpath):
@@ -14,28 +15,94 @@ def get_files_sizes(dirpath):
         break
     for i in range(len(fileList)):
         if ".csv" in fileList[i]:
-           csvFileFlag = True
+            csvFileFlag = True
         fileInfo = os.stat(path + "\\" + fileList[i])
         fileList[i] = fileList[i] + "--" + str(fileInfo.st_size) + "Kb"
 
     return fileList
-file = open("C:\\Users\\Alexandr\\Desktop\\_\\4 семестр УлГТУ ИВТ\\Технологии программирования\\lab2 Task3\\Clients.csv")
+
+
+###
+def sortedCSV(path, column):
+    result = ''
+    listString = []
+    with open(path) as File:
+        reader = csv.reader(File)
+        for row in reader:
+            listString.append(str(row).split(";"))
+    listString.pop(0)
+    outputList = sorted(listString, key=lambda x: x[column])
+    for i in outputList:
+        result = result + str(i) + "\n"
+    return result
+
+
+###
+def readCsv(path):
+    result = ""
+    with open(path) as File:
+        reader = csv.reader(File)
+        for row in reader:
+            result += (str(row) + "\n")
+    return result
+
+
+###
+def sortedByAttribute(path):
+    glistString = []
+    result = ''
+    with open(path) as File:
+        reader = csv.reader(File)
+        for row in reader:
+            glistString.append(str(row).split(";"))
+    glistString.pop(0)
+    lenMas = len(glistString)
+    iter = 0
+    while (iter < lenMas):
+        if 'true' not in glistString[iter]:
+            glistString.pop(iter)
+            lenMas -= 1
+            iter -= 1
+        iter += 1
+    for i in glistString:
+        result = result + str(i) + '\n'
+    return result
+
+
+##
+def createJson(string):
+    return 1
+
+
+path = "C:\\Users\\Alexandr\\Desktop\\_\\4 семестр УлГТУ ИВТ\\Технологии программирования\\lab2 Task3"
+csvFileFlag = False
 
 print("\n".join(get_files_sizes(path)))
 
 if csvFileFlag:
-    # fileCsv = input("Enter the CSV name of the file, wich you want to see:")
-    # print(path+"\\"+fileCsv)
-    table = np.genfromtxt(file, delimiter=";", dtype=None, names=True, encoding="utf8")
-    print(table)
-    # print(np.sort(table, axis=None))
-    typer = np.dtype([('№', 'U8'), ('date/time', 'U8'), ('output/input', 'U8'), ('sex', "U8")])
-    table2 = np.array(table, dtype=typer)
-    print(np.sort(table2, order='sex'))
+    fileCsv = path + "\\" + input("Enter the CSV name of the file, wich you want to see:")
+    # print(readCsv(fileCsv))
+    # print("You have 2 attempts for sorting thr csv file")
+    # for i in range(2):
+    #     column = int(input("Enter the column by wich you want to sorting:"))
+    #     print(sortedCSV(fileCsv, column))
+    print(sortedByAttribute(fileCsv))
+    jsonFile = sortedByAttribute(fileCsv)
+    chunks = re.split('\[\"\[\'|\", \'|\'\]\"\]\n|\'|, |\"', jsonFile)
+    iter = 0;
+    while iter != len(chunks):
+        if chunks[iter] == '':
+            chunks.pop(iter)
+            iter -= 1
+        iter += 1
+    iter = 0
+    output = []
+    while iter != len(chunks):
+        per = int(chunks[iter])
+        dic = {"№": per, "date/time": chunks[iter + 1], "output/input": bool(chunks[iter + 2]),
+               "sex": chunks[iter + 3]}
+        output.pop(dic)
+        iter += 3
 
-# type = np.dtype([('recruit_name', 'U8'), ('age', int),('weight', float), ('height', float)])
-# recruit = [('Вася', 18, 57.1, 1.96),('Петька', 25, 94.9, 1.51),('Сёмка', 30, 72.5, 1.83),('Антоха', 30, 146.8, 1.76)]
-# vdv = np.array(recruit, dtype=type)
-# print(vdv)
-# print(np.sort(vdv, order='recruit_name'))  # сортировка по весу
-
+    for i in output:
+        print(output[i])
